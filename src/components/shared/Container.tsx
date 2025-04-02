ab92186449192f8b65c80138e54d8a9309729089
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-04-02 11:51:38 +0000

    rendring children

--- src/components/shared/Container.tsx
+++ src/components/shared/Container.tsx
@@ -11,7 +11,7 @@ export default function Container({
     <div
       className={`mx-auto max-w-7xl w-full px-5 sm:px-8 md:px-14 lg:px-5 ${className}`}
     >
-      Container
+      {children}
     </div>
   );
 }
