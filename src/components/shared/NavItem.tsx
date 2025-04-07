b03e92a7d8a5fd07870a4414ed02d1cff775b97e
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-04-07 22:40:44 +0100

    formatting

--- src/components/shared/NavItem.tsx
+++ src/components/shared/NavItem.tsx
@@ -6,7 +6,10 @@ interface ContainerProps {
 export default function NavItem({ href, text }: ContainerProps) {
   return (
     <li>
-      <a href={href} className="duration-300 font-medium ease-linear hover:text-primary py-3">
+      <a
+        href={href}
+        className="duration-300 font-medium ease-linear hover:text-primary py-3"
+      >
         {text}
       </a>
     </li>
