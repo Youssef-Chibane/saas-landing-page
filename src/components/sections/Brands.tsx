bfac26218645a1aa82ebbf46f40dd844d2767ba9
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-04-12 16:05:08 +0100

    fix cantainer className

--- src/components/sections/Brands.tsx
+++ src/components/sections/Brands.tsx
@@ -4,7 +4,7 @@ import { Title } from "../shared/Title";
 export function Brands() {
   return (
     <section>
-      <Container className="space-x-8">
+      <Container className="space-y-8">
         <div className="text-center max-w-3xl mx-auto">
           <Title> Trusted by Industry Leaders </Title>
         </div>
