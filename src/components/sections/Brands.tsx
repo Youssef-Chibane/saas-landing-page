f270932dd59b9c89ad64e56ee3836fbf2993fd4d
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-04-12 16:04:05 +0100

    Brands section

---
+++ src/components/sections/Brands.tsx
@@ -0,0 +1,14 @@
+import { Container } from "../shared/Container";
+import { Title } from "../shared/Title";
+
+export function Brands() {
+  return (
+    <section>
+      <Container className="space-x-8">
+        <div className="text-center max-w-3xl mx-auto">
+          <Title> Trusted by Industry Leaders </Title>
+        </div>
+      </Container>
+    </section>
+  );
+}
